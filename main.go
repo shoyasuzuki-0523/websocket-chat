05c68a8e48a266979168981472cb9d098244f4ff
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2024-01-24 03:22:23 +0900

    試しにGoでチャットアプリ作成

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+func main() {
+	log.Println("Websocket App start.")
+
+	router := gin.Default()
+	m := melody.New()
+
+	rg := router.Group("/sampleapp")
+	rg.GET("/", func(ctx *gin.Context) {
+		http.ServeFile(ctx.Writer, ctx.Request, "templates/index.html")
+	})
+
+	rg.GET("/ws", func(ctx *gin.Context) {
+		m.HandleRequest(ctx.Writer, ctx.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		log.Printf("Broadcast. [session: %#v]\n", s)
+		m.Broadcast(msg)
+	})
+
+	m.HandleConnect(func(s *melody.Session) {
+		log.Printf("websocket connection open. [session: %#v]\n", s)
+	})
+
+	m.HandleDisconnect(func(s *melody.Session) {
+		log.Printf("websocket connection close. [session: %#v]\n", s)
+	})
+
+	router.Run()
+
+	fmt.Println("Websocket App End.")
+}
