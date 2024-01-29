90bf13edf343176ba23238f88e210070ddff6df6
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2024-01-29 12:26:19 +0900

    htmx化

--- main.go
+++ main.go
@@ -1,35 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
 
 var clients = make(map[*websocket.Conn]bool)
-
-// メッセージブロードキャストチャネル
 var broadcast = make(chan Message)
 
 type Message struct {
-	Type    int
-	Message []byte
+	Type    int    `json:"type"`
+	User    string `json:"user"`
+	Message string `json:"message"`
+}
+
+type Request struct {
+	Header map[string]string `json:"HEADERS"`
+	Input  string            `json:"input"`
+	User   string            `json:"user"`
 }
 
 func main() {
 	log.Println("Websocket App start.")
 
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 	wsupgrader := websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 	}
 
 	r.GET("/", func(ctx *gin.Context) {
-		http.ServeFile(ctx.Writer, ctx.Request, "templates/index.html")
+		rand.New(rand.NewSource(time.Now().UnixNano()))
+		name := fmt.Sprintf("Guest-%03d", rand.Intn(100))
+		ctx.HTML(http.StatusOK, "index.html", gin.H{"name": name})
 	})
 
 	r.GET("/ws", func(ctx *gin.Context) {
@@ -38,13 +49,22 @@ func main() {
 			log.Printf("Failed to set websocket upgrade: %+v\n", err)
 			return
 		}
+
 		clients[conn] = true
+
 		for {
-			t, msg, err := conn.ReadMessage()
+			t, raw, err := conn.ReadMessage()
 			if err != nil {
 				break
 			}
-			broadcast <- Message{Type: t, Message: msg}
+
+			var req Request
+			if err := json.Unmarshal(raw, &req); err != nil {
+				fmt.Println(err)
+			}
+
+			msg := fmt.Sprintf("<div hx-swap-oob=\"beforeend:#chat\"><span>%s[%s]> %s</span></br></div>", time.Now().Format("15:04:05"), req.User, req.Input)
+			broadcast <- Message{Type: t, User: req.User, Message: msg}
 		}
 	})
 	go handleMessages()
@@ -58,8 +78,7 @@ func handleMessages() {
 	for {
 		message := <-broadcast
 		for client := range clients {
-			err := client.WriteMessage(message.Type, message.Message)
-			if err != nil {
+			if err := client.WriteMessage(message.Type, []byte(message.Message)); err != nil {
 				log.Printf("error: %v", err)
 				client.Close()
 				delete(clients, client)
